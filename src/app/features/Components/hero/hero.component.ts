0c6e82ae76f141589a831fa87cbb1cbce1b53d90
gauravsingh-7217 <gaurav.singh@vidual.in>
2026-06-18 15:50:49 +0530

    Add navigation from home page to video chat screen

--- src/app/features/Components/hero/hero.component.ts
+++ src/app/features/Components/hero/hero.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { DataService } from '../../../core/services/data.service';
 import { GlobeVisualizationComponent } from '../globe-visualization/globe-visualization.component';
 import { LiveActivityComponent } from '../live-activity/live-activity.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
@@ -16,4 +17,10 @@ import { LiveActivityComponent } from '../live-activity/live-activity.component'
 })
 export class HeroComponent {
   protected data = inject(DataService);
+  constructor( private router:Router){
+   
+  }
+  goToVideoChat(): void {
+  this.router.navigate(['/video-chat']);
+}
 }
